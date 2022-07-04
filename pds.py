42830c030472d0b2a1ee248390849a9bb9462d75
Arne Sachtler <arne.sachtler@dlr.de>
2022-07-04 19:40:31 +0200

    PDS

--- pds.py
+++ pds.py
@@ -1,10 +1,12 @@
 from abc import abstractmethod
 from itertools import chain, islice, repeat
+import numpy as np
 
 
 class PlanarDynamicalSystem:
     def __init__(self, n_dof):
         self.dof = n_dof
+        self._torque_sources = []
 
     def mass_matrix(self, q): raise NotImplementedError
 
@@ -26,3 +28,11 @@ class PlanarDynamicalSystem:
 
     def jacobi_metric(self, q, E): raise NotImplementedError
 
+    def add_torque_source(self, torque_source):
+        self._torque_sources.append(torque_source)
+
+    def _custom_torque(self, t, q, dq):
+        if len(self._torque_sources) > 0:
+            return np.sum([c(t, q, dq) for c in self._torque_sources], axis=0)
+        else:
+            return np.zeros(self.dof)

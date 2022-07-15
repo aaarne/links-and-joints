f6fdc4aab54850ba26d60d30d3dbe30ba7b37457
Arne Sachtler <arne.sachtler@gmail.com>
2022-07-15 17:18:36 +0200

    Add stuff

--- controllers/conversative.py
+++ controllers/conversative.py
@@ -4,13 +4,15 @@ import numpy as np
 
 def make_controller_conservative(controller):
     def conservative_controller(t, q, dq):
-        n = len(q)
-        return qpsolvers.solve_qp(
-            P=np.eye(n),
-            q=-2*controller(t, q, dq),
-            A=dq,
-            b=np.array([0]).reshape((1, 1)),
-            solver='quadprog'
-        )
+        if np.linalg.norm(dq) < 1e-3:
+            return controller(t, q, dq)
+        else:
+            return qpsolvers.solve_qp(
+                P=np.eye(len(q)),
+                q=-2*controller(t, q, dq),
+                A=dq,
+                b=np.array([0.0]),
+                solver='cvxopt',
+            )
 
     return conservative_controller

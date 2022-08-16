0855849f132d7f5ce32ac4426a42d1f787fad78e
Arne Sachtler <arne.sachtler@gmail.com>
2022-08-16 19:03:32 +0200

    Generate RRR model

--- planar_dynamical_system/__init__.py
+++ planar_dynamical_system/__init__.py
@@ -1,3 +1,4 @@
 from .generated.rr import RR as DoublePendulum
 from .generated.xr import XR as PendulumOnCart
 from .generated.ry import RY as SLIP
+from .generated.rrr import RRR as ThreeDofPlanarRobot

f7fd48e350c652aefc64fbd522cea5c6b570eb31
Arne Sachtler <arne.sachtler@gmail.com>
2022-10-22 16:03:19 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name="links-and-joints",
+    version="0.0.1",
+    author="Arne Sachtler",
+    author_email="arne.sachtler@tum.de",
+    description="Dynamical Models of Planar Mechanical Systems",
+    url="https://github.com/aaarne/links-and-joints",
+    packages=['links_and_joints'],
+    install_requires=['sympy'],
+)
